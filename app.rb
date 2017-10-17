73a53d32159580a5d587ddc38487376074422166
Michel Silva <costa.mch@gmail.com>
2017-10-17 13:24:42 -0200

    Dockering app and migration create_faqs

--- app.rb
+++ app.rb
@@ -5,7 +5,7 @@ require 'sinatra/activerecord'
 require './config/database'
 
 class App < Sinatra::Base
-  get '/' do
-    'Hello world!'
+  get '/sinatra' do
+    'Hello world! Sinatra'
   end
 end
